5ed0f15083a587078bbd1c08ef920d4e77dc3902
matievisthekat <matievisthekat@gmail.com>
2023-10-08 20:29:22 +0200

    get users preferred language

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import i18nConfig from '../next-i18next.config.js';
 import Head from '../components/Head';
 import Layout from '../components/layouts';
+import {useEffect, useState} from 'react';
 
 interface Props extends SSRConfig {}
 
@@ -14,9 +15,22 @@ export const getStaticProps: GetStaticProps<Props> = async ({locale}) => ({
 });
 
 export default function Home(props: InferGetStaticPropsType<typeof getStaticProps>) {
+	const [prefLang, setPrefLang] = useState(i18nConfig.i18n.defaultLocale);
+
+	useEffect(() => {
+		const userLang = window.navigator.language || window.navigator.languages[0];
+		const matchLang = i18nConfig.i18n.locales.find(
+			(l) =>
+				l === userLang || l.toLowerCase().replace('-', '_').split('_')[0] === userLang.toLowerCase().split('_')[0]
+		);
+		if (matchLang) {
+			setPrefLang(matchLang.replace('-', '_'));
+		}
+	}, []);
+
 	return (
 		<Layout {...props}>
 			<Head />
 		</Layout>
 	);
-};
+}
