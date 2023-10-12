86715a1b923691f39a7e0be40ddf4b33754b91f5
matievisthekat <matievisthekat@gmail.com>
2023-10-12 17:56:04 +0200

    add oregon chair to gallery

--- pages/galleryCards.tsx
+++ pages/galleryCards.tsx
@@ -1,13 +1,20 @@
 import cedarCandleHolderTall from '../public/img/gallery/cedar-candleholder-tall.png';
+import oregonPineChair from '../public/img/gallery/oregon-pine-chair.png';
 
 const gallery = [
 	{
 		title: 'Cedar Candle Holder Set',
 		slug: 'cedar-candle-holders',
-		shortDesc:
-			'A set of large candle holders made from sweet sweet Cedar wood. The scent of the wood itself is good enough to not need scented candles!',
+		shortDesc: 'A set of large candle holders made from aromatic Cedar wood',
 		img: cedarCandleHolderTall,
 		alt: 'Purple cedar wood candle holder'
+	},
+	{
+		title: 'Oregon Pine Chair',
+		slug: 'oregon-pine-chair',
+		shortDesc: 'Made from recovered Oregon Pine, this chair involved a lot of complicated joinery',
+		img: oregonPineChair,
+		alt: 'Upholstered chair made from Oregon Pine'
 	}
 ];
 
